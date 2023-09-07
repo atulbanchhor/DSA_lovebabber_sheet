d1aa7f790c9d872173d9c429126138b66c7eaf8f
Atul Banchhor <99315260+atulbanchhor@users.noreply.github.com>
2023-09-07 14:40:17 +0530

    Reverse_Array.java

--- Reverse_Array.java
+++ Reverse_Array.java
@@ -2,12 +2,12 @@ package Love_Babber_Sheet;
 
 public class Reverse_Array {
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5,6};
+        int arr[] = {1, 2, 3, 4, 5,6};               // isme mai array k length ko divide kr ra aura agar vo positve hai to uske lea alag aur negative k leav alag
         int temp = 0;
         int k = arr.length / 2;
         int l = arr.length - 1;
 
-        if (k / 2 == 0) {
+        if (k / 2 == 0) {                            // for positive 
             for (int i = 0; i <= k; i++) {
                 temp = arr[i];
                 arr[i] = arr[l];
@@ -15,7 +15,7 @@ public class Reverse_Array {
                 l--;
             }
         }else {
-            for (int i = 0; i < k ; i++) {
+            for (int i = 0; i < k ; i++) {            // for negative
                 temp = arr[i];
                 arr[i] = arr[l];
                 arr[l] = temp;
